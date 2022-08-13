92f7e09934fe5e6e4df72557329852f5a7b6144a
Nazima17 <egemberdinaz17@mail.ru>
2022-08-13 19:58:26 +0600

    d

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,23 @@ public class Main {
         boss.setDamage(20);
         boss.setHealth(600);
         boss.setDefence("Kinetic");
-        System.out.println("boss damage: "+boss.getDamage() );
-System.out.println("boss health : " + boss.getHealth());
-System.out.println("boss defence :"+boss.getDefence());
+        System.out.println("boss damage: " + boss.getDamage());
+        System.out.println("boss health : " + boss.getHealth());
+        System.out.println("boss defence :" + boss.getDefence());
+createHeroes();
+        for (int i = 0; i < createHeroes().length; i++) {
+            System.out.println( "Damage hero : "+createHeroes()[i].getDemage()+ "  "+ " Helth: " +createHeroes()[i
+                    ].getHealth()+" "+"Superower: "+createHeroes()[i].getSuperPower());
+
+        }
+    }
+    public static Hero[]createHeroes (){
+        Hero megare  = new Hero (270,33,"swipe");
+        Hero aid =new Hero(250,48);
+        Hero zoro = new Hero(210,22);
+        Hero[]heroes={megare,aid,zoro};
+        return heroes;
+
 
     }
 }
